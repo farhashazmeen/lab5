433be490fe510a9fc053595fa6f8b87f7fda1434
farhashazmeen <farhashazmeen@gmail.com>
2017-10-01 13:20:39 +0200

    first commit

---
+++ lab5/R/geo1.R
@@ -0,0 +1,52 @@
+
+# This function queries Google's geocoding API
+
+#' Title
+#'
+#' @param address 
+#'
+#' @return A data.frame containing the resulting \code{longitude/s}, \code{latitude/s} and \code{address/es}
+#' @export
+#'
+#' @examples geo_connect("delhi")
+geo_connect <- function(address){
+  
+  library(RCurl)
+  library(rjson)
+  
+  # Call the function URLencode to build a URL string
+  
+  URL <- URLencode(paste("http://maps.google.com/maps/api/geocode/json?address=", address, sep=""))
+  
+  # Request information from the API   and Parse the JSON
+  
+  URL.data <- fromJSON(getURL(URL))
+  
+  # Check the response status from the API
+  
+  if (URL.data$status == "OK")
+    {
+    
+    # Extract the latitude and longitude coordinates and address
+    
+    latitude <- URL.data$results[[1]]$geometry$location$lat
+    longitude <- URL.data$results[[1]]$geometry$location$lng
+    formatted_address <- URL.data$results[[1]]$formatted_address
+    # return(paste(latitude, longitude,formatted_address, sep = ", "))
+    }
+    else
+    {
+    
+    # Return Error
+      
+    latitude <- 0
+    longitude <- 0
+    formatted_address <- "ERROR"
+    } 
+     address.data <- data.frame(latitude = latitude, longitude = longitude, address = formatted_address, stringsAsFactors = FALSE)
+
+     return(address.data)
+}
+#geo_connect("27.17502, 78.04216")
+geo_connect("linkoping")
+
---
+++ lab5/tests/testthat/test_geo.R
@@ -0,0 +1,36 @@
+# testing 
+
+library(testthat)
+library(ggmap)
+
+# testing the inputs and outputs of our functions
+expect_true(is.data.frame(geo_connect("Madrid")))
+expect_true(is.data.frame(geo_connect_vector(c("Madrid", "Linkoping"))))
+
+
+# geo_connect()
+
+test_that("testing function geo_connect",{
+  # address 
+  expect_equal(geo_connect("Stockholm"), 
+               data.frame(latitude = 59.32932, 
+                          longitude = 18.06858,
+                          address = "Stockholm, Sweden",
+                          stringsAsFactors = FALSE),
+               tolerance = 0.00001)
+  # coordenates
+  expect_equal(geo_connect("27.17502, 78.04216"), 
+               data.frame(latitude = 27.17518 , 
+                          longitude = 78.04685,
+                          address = "Dashehara Ghat Rd, Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282006, India",
+                          stringsAsFactors = FALSE),
+               tolerance = 0.00001)
+  # address ERROR         
+  expect_equal(geo_connect("34.7ssfad"), 
+               data.frame(latitude=0, 
+                          longitude=0, 
+                          address="ERROR", 
+                          stringsAsFactors = FALSE))
+})
+
+
